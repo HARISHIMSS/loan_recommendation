631502723480aec44ecbbd1920955505254b8a89
HARISHIMSS <harishm@imss.co.in>
2024-12-13 16:32:25 +0530

    requirements.txt added

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = FastAPI(
     summary = "Loan Recommendation Engine",
     description = "Loan Recommendation Engine",
     version="0.0.1",
+    docs_url="/docs",
     openapi_url="/openapi.json",
     root_path="/api/v1"
 )
