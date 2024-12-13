d8062d840792270b6e75df92f9ed040ef57efb5f
HARISHIMSS <harishm@imss.co.in>
2024-12-13 17:21:55 +0530

    requirements.txt added

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ app = FastAPI(
     summary = "Loan Recommendation Engine",
     description = "Loan Recommendation Engine",
     version="0.0.1",
-    root_path="/api/v1",
+    root_path="/loan-recommendation/api/v1",
     docs_url="/docs",
-    openapi_url="/loan-recommendation/api/v1/openapi.json"
 )
 
 @app.get("/get_priority_filters")
